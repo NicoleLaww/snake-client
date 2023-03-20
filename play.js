3ca3ed941fccc6dd72fb01a9d92b90475d263cd6
Nicole Law <nicole.law12@gmail.com>
2023-03-20 18:38:05 -0400

    input module

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const connect = require("./client");
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const setupInput = require("./input");
 
 
 console.log("Connecting ...");
-setupInput();
 connect();
+
+setupInput();
