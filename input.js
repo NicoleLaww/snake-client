88e8d5f71e99566bca97352586b1f05e6df05b81
Nicole Law <nicole.law12@gmail.com>
2023-03-20 19:01:59 -0400

    WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,7 +14,35 @@ const setupInput = function() {
     }
   };
 
+  const up = function(key) {
+    if (key === "w") {
+      conn.write("Move: up");
+    }
+  };
+
+  const left = function(key) {
+    if (key === "a") {
+      conn.write("Move: left");
+    }
+  };
+
+  const down = function(key) {
+    if (key === "s") {
+      conn.write("Move: down");
+    }
+  };
+
+  const right = function(key) {
+    if (key === "d") {
+      conn.write("Move: right");
+    }
+  };
+
   stdin.on("data", handleUserInput);
+  stdin.on("data", up);
+  stdin.on("data", down);
+  stdin.on("data", left);
+  stdin.on("data", right);
 
   return stdin;
 };
